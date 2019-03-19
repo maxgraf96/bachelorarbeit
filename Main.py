5465ad7125a2f2d2d0a774de7d4760e88bc4db81
max <max.graf@live.at>
2019-03-19 19:13:32 +0100

    Update dependencies

--- Main.py
+++ Main.py
@@ -5,10 +5,9 @@ from pathlib import Path
 from pygame import mixer
 import time
 import radiorec
-import librosa
 import pyAudioAnalysis.audioBasicIO as abio
 
-#aT.featureAndTrain(["data/music","data/speech/little"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False)
+#aT.featureAndTrain(["data/music/verylittle","data/speech/verylittle"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False)
 
 # measure time
 start = time.time()
@@ -27,12 +26,6 @@ while i < 10:
     radiorec.my_record(url, 3.0, currentFile)
     path = "data/test/" + currentFile + ".mp3"
     result = aT.fileClassification(path, "svmSMtemp", "svm")
-
-
-    # MFCC
-    np_dat = abio.stereo2mono(abio.readAudioFile(path)[1]).astype("float32")
-    #mfcc = librosa.feature.mfcc(np_dat)
-    #print(mfcc)
 
     music_speech_tuple = result[1]
     if music_speech_tuple[0] > music_speech_tuple[1]:
